4c825c44212bc9a8f758a8a17989c4192317de05
Cristian Espes <christian_zgza@hotmail.com>
2019-12-01 13:10:43 +0100

    First commit

---
+++ TestInnocv/Entities/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  TestInnocv
+//
+//  Created by CRISTIAN ESPES on 01/12/2019.
+//  Copyright © 2019 Cristian Espes. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+  let id: Int?
+  var name: String?
+  var birthdate: Date?
+}
---
+++ TestInnocv/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestInnocv
+//
+//  Created by CRISTIAN ESPES on 01/12/2019.
+//  Copyright © 2019 Cristian Espes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
